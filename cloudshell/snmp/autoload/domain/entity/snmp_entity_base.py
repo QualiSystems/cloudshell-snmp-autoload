cd6f0e64bc7306095f2daba0f08995c2185f126f
Dmytro Dovbii <dmanddov@gmail.com>
2020-06-22 12:16:25 +0300

    Python3 support: iteritems() -> items()
    iteritems() doesn't work in python 3.x, so must be
    replaced with items()

--- cloudshell/snmp/autoload/domain/entity/snmp_entity_base.py
+++ cloudshell/snmp/autoload/domain/entity/snmp_entity_base.py
@@ -100,7 +100,7 @@ class BaseEntity(object):
             if not self.vendor_type:
                 return ""
             entity_class = entity_class.replace("'", "")
-            for key, value in ENTITY_VENDOR_TYPE_TO_CLASS_MAP.iteritems():
+            for key, value in ENTITY_VENDOR_TYPE_TO_CLASS_MAP.items():
                 if key.search(self.vendor_type.lower()):
                     entity_class = value
         if ENTITY_TO_CONTAINER_PATTERN.search(self.vendor_type):
