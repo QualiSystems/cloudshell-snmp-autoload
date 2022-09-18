1c1d303b814e5aa707fcc06b5c48cd508fd6f044
Costya <costya.y@qualisystems.com>
2022-09-18 19:36:26 +0300

    updated tests

--- tests/cloudshell/snmp/autoload/helper/test_module_helper.py
+++ tests/cloudshell/snmp/autoload/helper/test_module_helper.py
@@ -5,71 +5,86 @@ from cloudshell.shell.standards.autoload_generic_models import GenericPort
 from cloudshell.shell.standards.networking.autoload_model import NetworkingResourceModel
 from cloudshell.snmp.autoload.helper.module_helper import ModuleHelper
 from cloudshell.snmp.autoload.services.physical_entities_table import PhysicalTable
-from cloudshell.snmp.autoload.snmp.tables.snmp_entity_table import SnmpEntityTable
-from cloudshell.snmp.core.domain.quali_mib_table import QualiMibTable
-
-from tests.cloudshell.snmp.autoload.data.physical_entities_data import (
-    MOCK_SNMP_RESPONSE,
-)
 
 
 class TestPhysicalTable(TestCase):
-    def setUp(self) -> None:
-        self._logger = Mock()
-        entity_table = self._create_entity_table()
-        self._resource_model = NetworkingResourceModel(
+    def test_get_parent_module_no_entity_no_entity_no_module_map(self):
+        resource_model = NetworkingResourceModel(
             "Resource Name",
             "Shell Name",
             "CS_Switch",
         )
-        self.table = PhysicalTable(entity_table, self._logger, self._resource_model)
-        self.table.MODULE_EXCLUDE_LIST = [
-            r"powershelf|cevsfp|cevxfr|cevSensor|cevCpuTypeCPU|"
-            r"cevxfp|cevContainer10GigBasePort|cevModuleDIMM|"
-            r"cevModulePseAsicPlim|cevModule\S+Storage$|"
-            r"cevModuleFabricTypeAsic|cevModuleCommonCardsPSEASIC|"
-            r"cevFan|cevCpu|cevSensor|cevContainerDaughterCard"
-        ]
-
-    def _create_module_helper(self):
-        return ModuleHelper(
-            physical_table_service=self.table,
-            resource_model=self._resource_model,
-            logger=self._logger,
-        )
-
-    def _create_entity_table(self):
-        snmp = Mock()
-        response = QualiMibTable("test")
-        response.update(MOCK_SNMP_RESPONSE)
-        snmp.get_multiple_columns.return_value = response
-
-        return SnmpEntityTable(snmp, self._logger)
-
-    def test_get_parent_entity_by_ids_known_1_module(self):
-        result = self._create_module_helper().get_parent_module("0-8")
+        entity_table = Mock()
+        entity_table.physical_structure_snmp_table = {}
+        table = PhysicalTable(entity_table, Mock(), resource_model)
+        result = ModuleHelper(
+            physical_table_service=table,
+            resource_model=resource_model,
+            logger=Mock(),
+        ).get_parent_module("0-8")
         assert str(result.relative_address) == "CH0/M0/SM8"
 
-    def test_get_parent_entity_by_ids_known_2_modules(self):
-        module_helper = self._create_module_helper()
-        result = module_helper.get_parent_module("0-8-1")
-        assert str(result.relative_address) == "CH0/M8/SM1"
+    def test_get_parent_module_no_entity_no_entity_sub_module_map(self):
+        resource_model = NetworkingResourceModel(
+            "Resource Name",
+            "Shell Name",
+            "CS_Switch",
+        )
+        entity_table = Mock()
+        entity_table.physical_structure_snmp_table = {}
+        table = PhysicalTable(entity_table, Mock(), resource_model)
+        helper = ModuleHelper(
+            physical_table_service=table,
+            resource_model=resource_model,
+            logger=Mock(),
+        )
+        chassis = resource_model.entities.Chassis("0")
+        module = resource_model.entities.Module("0")
+        chassis.connect_module(module)
+        sub_module = resource_model.entities.SubModule("8")
+        module.connect_sub_module(sub_module)
+        helper._sub_module_parents_map["0-0-8"] = sub_module
+        result = helper.get_parent_module("0-8")
+        assert str(result.relative_address) == "CH0/M0/SM8"
+        assert result == sub_module
 
-    def test_get_parent_entity_by_ids_unknown_2_modules(self):
-        result = self._create_module_helper().get_parent_module("0-9-1")
-        assert str(result.relative_address) == "CH0/M9/SM1"
+    def test_get_parent_module_no_entity_no_entity_module_map(self):
+        resource_model = NetworkingResourceModel(
+            "Resource Name",
+            "Shell Name",
+            "CS_Switch",
+        )
+        entity_table = Mock()
+        entity_table.physical_structure_snmp_table = {}
+        table = PhysicalTable(entity_table, Mock(), resource_model)
+        helper = ModuleHelper(
+            physical_table_service=table,
+            resource_model=resource_model,
+            logger=Mock(),
+        )
+        chassis = resource_model.entities.Chassis("0")
+        module = resource_model.entities.Module("0")
+        chassis.connect_module(module)
+        helper._module_parents_map["0"] = module
+        sub_module = resource_model.entities.SubModule("8")
+        module.connect_sub_module(sub_module)
+        result = helper.get_parent_module("0-8")
+        assert str(result.relative_address) == "CH0/M0/SM8"
 
-    def test_get_parent_entity_by_ids_known_1_module_no_chassis(self):
-        result = self._create_module_helper().get_parent_module("8")
-        assert str(result.relative_address) == "CH0/M8"
-
-    def test_get_parent_entity_by_ids_unknown_2_modules_no_chassis(self):
-        result = self._create_module_helper().get_parent_module("9-1")
-        assert str(result.relative_address) == "CH0/M9/SM1"
-
-    def test_attach_port_to_parent(self):
-        _ = self.table.physical_structure_table
-        module_helper = self._create_module_helper()
+    def test_attach_port_to_parent_sub_module(self):
+        resource_model = NetworkingResourceModel(
+            "Resource Name",
+            "Shell Name",
+            "CS_Switch",
+        )
+        entity_table = Mock()
+        entity_table.physical_structure_snmp_table = {}
+        table = PhysicalTable(entity_table, Mock(), resource_model)
+        module_helper = ModuleHelper(
+            physical_table_service=table,
+            resource_model=resource_model,
+            logger=Mock(),
+        )
         interface = GenericPort("4324")
         entity_port = GenericPort("4105")
         module_helper.attach_port_to_parent(
@@ -77,3 +92,25 @@ class TestPhysicalTable(TestCase):
         )
         assert str(interface.relative_address.parent_node) == "CH0/M8/SM0"
         assert str(interface.relative_address) == "CH0/M8/SM0/P4324"
+
+    def test_attach_port_to_parent_module(self):
+        resource_model = NetworkingResourceModel(
+            "Resource Name",
+            "Shell Name",
+            "CS_Switch",
+        )
+        entity_table = Mock()
+        entity_table.physical_structure_snmp_table = {}
+        table = PhysicalTable(entity_table, Mock(), resource_model)
+        module_helper = ModuleHelper(
+            physical_table_service=table,
+            resource_model=resource_model,
+            logger=Mock(),
+        )
+        interface = GenericPort("4324")
+        entity_port = GenericPort("4105")
+        module_helper.attach_port_to_parent(
+            entity_port=entity_port, if_port=interface, port_id="8"
+        )
+        assert str(interface.relative_address.parent_node) == "CH0/M8"
+        assert str(interface.relative_address) == "CH0/M8/P4324"
