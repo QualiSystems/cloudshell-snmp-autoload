688b9061890e2ce5a34708cc666b2451ae329a5a
Kyrylo Maksymenko <saklar13@gmail.com>
2023-06-02 17:01:03 +0200

    when creating chassis check that this object don't have another chassis as parent

--- cloudshell/snmp/autoload/services/physical_entities_table.py
+++ cloudshell/snmp/autoload/services/physical_entities_table.py
@@ -127,8 +127,20 @@ class PhysicalTable:
             self._add_port(entity)
         elif "powersupply" in entity_class.lower():
             self._add_power_port(entity)
-        elif "chassis" in entity_class.lower():
+        elif "chassis" in entity_class.lower() and not self._has_parent_chassis(entity):
             self._add_chassis(entity)
+
+    def _has_parent_chassis(self, entity: BaseEntity) -> bool:
+        if self.entity_table.physical_structure_snmp_table.get(entity.parent_id):
+            parent = self.load_entity(entity.parent_id)
+            entity_class = self.chassis_helper.get_physical_class(entity)
+            if "chassis" in entity_class.lower():
+                res = True
+            else:
+                res = self._has_parent_chassis(parent)
+        else:
+            res = False
+        return res
 
     def _add_chassis(self, entity):
         index = "0" if entity.position_id == "-1" else entity.position_id
