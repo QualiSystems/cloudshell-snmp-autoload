cc16faf896987efa48cdd04cdea952f13fe6c02b
Costya <costya.y@qualisystems.com>
2023-06-22 08:23:14 -0500

    modified vendor_type_label property

--- cloudshell/snmp/autoload/snmp/entities/snmp_entity_base.py
+++ cloudshell/snmp/autoload/snmp/entities/snmp_entity_base.py
@@ -1,4 +1,4 @@
-from functools import cached_property
+import re
 
 from cloudshell.snmp.autoload.constants.entity_constants import (
     ENTITY_CLASS,
@@ -66,14 +66,9 @@ class BaseEntity:
         result = self.entity_row_response.get(ENTITY_VENDOR_TYPE.object_name)
         return result.safe_value if result else ""
 
-    @cached_property
+    @property
     def vendor_type_label(self):
-        snmp_response = self.entity_row_response.get(ENTITY_VENDOR_TYPE.object_name)
-        result = snmp_response.safe_value if snmp_response else ""
-        try:
-            result = snmp_response.object_type[1].getLabel()[-1]
-        finally:
-            return result
+        return re.sub(r"^.+::", "", self.vendor_type)
 
     @property
     def model(self):
